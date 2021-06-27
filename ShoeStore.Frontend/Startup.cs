4a4332442fc579d0e1e26ee4707df4518d4341fd
shifatullah <5946849+shifatullah@users.noreply.github.com>
2021-06-27 20:33:23 +1000

    migrated to .netcore 3.0 from 2.1

--- ShoeStore.Frontend/Startup.cs
+++ ShoeStore.Frontend/Startup.cs
@@ -29,10 +29,9 @@ namespace ShoeStore.Frontend
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            });            
 
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,11 +51,11 @@ namespace ShoeStore.Frontend
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {                
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
